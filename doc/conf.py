16da84079bced43af155568b165ca484ce38fcb1
Zhang Jingqiang <zh_jq@outlook.com>
2024-12-02 22:50:28 +0800

    prepare to upload docs to Read the Docs

---
+++ doc/conf.py
@@ -0,0 +1,18 @@
+# File: docs/conf.py
+
+extensions = [
+    "multiproject",
+]
+
+# Define the projects that will share this configuration file.
+multiproject_projects = {
+    "g3proxy": {
+        "path": "g3proxy",
+    },
+    "g3tiles": {
+        "path": "g3tiles",
+    },
+}
+
+# Common options.
+html_theme = "sphinx_rtd_theme"
